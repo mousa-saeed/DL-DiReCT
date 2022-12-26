a48f6a28dcdeb5cfc574e5fc437e2e93c7a4a5f6
Mousa <mousa.saeed.m@gmail.com>
2022-12-26 21:55:28 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.write('# Hello World from 1littlecoder, please subscribe')
+st.write('## Run Streamlit on Colab with `pyngrok` ')
+st.write("This is an amazing tutorial, I love this channel!!!")
+st.markdown("# Don't forget to subscribe")
