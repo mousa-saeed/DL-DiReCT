a2fcad23d8550f5e4c581df6c13a670ecb131076
Mousa <mousa.saeed.m@gmail.com>
2022-12-26 22:12:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ import sys
 
 log = subprocess.Popen([f'{sys.executable} -m pip install -e .'], stdout = subprocess.PIPE, stderr= subprocess.PIPE, shell=True)
   # wait for subprocess to install package before running your actual code below
-time.sleep(90)
+# time.sleep(90)
 
 
 
 
 st.write(print(log) )
+st.file_uploader()
 # st.write('## Run Streamlit on Colab with `pyngrok` ')
 # st.write("This is an amazing tutorial, I love this channel!!!")
 st.markdown("# maybe it worked")
