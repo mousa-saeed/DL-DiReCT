a3e7f10734e535add030290aeca05a696d283bad
mousa-saeed <mousa.saeed.m@gmail.com>
2023-02-20 21:53:25 +0100

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,76 @@
+import dicom2nifti
 import streamlit as st
 import subprocess
+# import pydicom
+import nibabel as nib
 import torch
 import time
 import sys
 import os
 
-gpu_available = torch.cuda.is_available()
 
+dicom_folder = './dicom'
+nifti_folder = './nifti'
+
+
+IN_VOLUME="dest/in_volume.nii.gz"
+BET_INPUT_VOLUME="./T1w_norm.nii.gz"
+MASK_VOLUME="./T1w_norm_noskull_mask.nii.gz"
+
+IN_VOLUME_CROP="dest/T1w_norm_noskull_cropped.nii.gz"
+MASK_VOLUME ="dest/in_volume_mask.nii.gz"
+
+LOW_MEM_ARG = "--lowmem True" 
+SUBJECT_ID = st.text_input('Subject ID', 'sub-001')
+
+THICK_VOLUME= "dest/T1w_thickmap.nii.gz"
+
+
+# Create destination folder if it doesn't exist
+if not os.path.exists(dicom_folder):
+    os.makedirs(dicom_folder)
+
+if not os.path.exists(nifti_folder):
+    os.makedirs(nifti_folder)
+
+if not os.path.exists("dest"):
+    os.makedirs('dest')
+
+uploaded_files = st.file_uploader('Choose files', accept_multiple_files=True)
+
+# Save uploaded files to destination folder
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        file_path = os.path.join(dicom_folder, uploaded_file.name)
+        with open(file_path, 'wb') as f:
+            f.write(uploaded_file.read())
+       
+# making sure the dicom folder isnt emptpy       
+if not os.listdir(dicom_folder):
+    st.write(f'{dicom_folder} is empty')
+else:
+    st.write(f'{dicom_folder} is not empty')
+
+
+st.write("="*50)
+
+st.title('DICOM to NIfTI Converter')
+
+if st.button("Convert"):
+    st.write(f'Converting DICOM files in {dicom_folder}...')
+
+    # Convert to NIfTI
+    dicom2nifti.convert_directory(dicom_folder, nifti_folder, compression=True, reorient=True)
+    
+    # Save NIfTI file
+    # nifti_filename = f'{output_file}.nii.gz'
+    st.write(os.listdir(nifti_folder))
+    st.success(f'Conversion complete!')
+
+st.write("="*50)
+
+
+gpu_available = torch.cuda.is_available()
 
 st.write("# Cortex Measuring App")
 
@@ -15,6 +79,34 @@ if gpu_available == False:
   st.write("The App will take take longer time than with GPU")
 
 st.write("="*50)
+
+
+if st.button('do all'):
+    uploaded_file = os.listdir(nifti_folder)[0]
+    st.write(f"file is {uploaded_file}")
+    st.write(subprocess.run([f"{sys.executable}", "src/conform.py", uploaded_file, "./T1w_norm.nii.gz" ],
+                          stdout = sys.stdout, stderr= subprocess.PIPE) )
+    st.write("Done conforming")
+    st.write(subprocess.run([f"{sys.executable} src/bet.py {BET_INPUT_VOLUME} {IN_VOLUME} "],
+                          stdout = sys.stdout, stderr= subprocess.PIPE, shell = True) )
+    st.write("Done Skull stripping")
+    st.write(subprocess.run([f"{sys.executable} src/crop.py {MASK_VOLUME} {IN_VOLUME} {IN_VOLUME_CROP}"],
+                        stdout= sys.stdout, stderr = subprocess.PIPE, shell = True) ) 
+    st.write(subprocess.run([f"{sys.executable} src/DeepSCAN_Anatomy_Newnet_apply.py {LOW_MEM_ARG} {IN_VOLUME_CROP} dest/ {SUBJECT_ID}"] ,
+            stdout= sys.stdout, stderr = subprocess.PIPE, shell = True) ) 
+    st.write(subprocess.run([f"{sys.executable} src/DiReCT.py dest/ dest/ "], 
+          stdout= sys.stdout, stderr = subprocess.PIPE, shell = True) ) 
+    st.write("Done DiReCT")
+
+    # st.write(subprocess.run([f"{sys.executable} src/extract_stats.py {THICK_VOLUME} dest/seg.nii.gz dest/softmax_seg.nii.gz {SUBJECT_ID}"],
+    #           stdout= sys.stdout, stderr = subprocess.PIPE, shell = True) ) 
+
+    st.write(os.listdir())
+    st.write(os.listdir("dest"))
+
+    
+st.write("="*50)
+
 
 uploaded_file = st.file_uploader("Upload a Nifti file")
 if uploaded_file != None:
@@ -28,25 +120,14 @@ if uploaded_file != None:
 
   with open(path,"wb") as f:
           f.write(uploaded_file.getbuffer())
+   #===================
 
-  # st.write('## Run Streamlit on Colab with `pyngrok` ')
-  # st.write("This is an amazing tutorial, I love this channel!!!")
-  # st.write(subprocess.run(["dl+direct"] ))
-  if st.button('use bash'):
-    st.write(subprocess.run([f"bash dl+direct.sh --subject 001 --lowmem --bet ./{uploaded_file.name} dest"],
-         stdout= sys.stdout, stderr = sys.stderr, shell = True) ) 
-         
   if st.button("Conform Nifti"):
     st.write(subprocess.run([f"{sys.executable}", "src/conform.py", uploaded_file.name, "./T1w_norm.nii.gz" ],
                             stdout = sys.stdout, stderr= subprocess.PIPE) )
     st.write("Done conforming")
 
   
-  IN_VOLUME="dest/in_volume.nii.gz"
-  BET_INPUT_VOLUME="./T1w_norm.nii.gz"
-  MASK_VOLUME="./T1w_norm_noskull_mask.nii.gz"
-
-
   if st.button("Skull Strip", key = 'abc'):
     st.write(subprocess.run([f"{sys.executable} src/bet.py {BET_INPUT_VOLUME} {IN_VOLUME} "],
                             stdout = sys.stdout, stderr= subprocess.PIPE, shell = True) )
